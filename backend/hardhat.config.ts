b52f0eeff13b6c87aaa1de9d482d372cc915eae0
gracekarinn <gracekarina12@gmail.com>
2025-02-04 23:34:27 +0700

    feat: update env

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,11 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
     solidity: "0.8.19",
     networks: {
         mantaTest: {
-            url: process.env.RPC_URL,
+            url: process.env.MANTA_RPC_URL,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
         },
     },
