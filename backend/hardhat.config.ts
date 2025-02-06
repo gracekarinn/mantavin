126947bb17e88fd8a66701aa18bd5ee42fb45f9e
gracekarinn <gracekarina12@gmail.com>
2025-02-06 20:16:22 +0700

    feat: update hardhat configuration

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,19 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const config: HardhatUserConfig = {
     solidity: "0.8.19",
     networks: {
-        mantaTest: {
-            url: process.env.MANTA_RPC_URL,
+        mantaSepolia: {
+            url: "https://pacific-rpc.sepolia-testnet.manta.network/http",
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+            chainId: 3441006,
+            gasPrice: "auto",
+            gas: "auto",
+            timeout: 20000,
         },
     },
     paths: {
-        artifacts: "./src/artifacts",
+        sources: "./contracts",
+        artifacts: "./artifacts",
     },
 };
 
