87c48c546e6fa7bfcb02d072a6bb9cdfab5a6a44
gracekarinn <gracekarina12@gmail.com>
2025-02-10 21:45:44 +0700

    feat: sorry kehapus

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -1,3 +1,16 @@
+export interface UserPayload {
+    id: string;
+    email: string;
+    role: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserPayload;
+        }
+    }
+}
 export interface RegisterBody {
     email: string;
     password: string;
