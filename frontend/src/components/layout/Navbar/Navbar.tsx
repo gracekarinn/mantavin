117b096c76678e3a0ee5e12ce9c477d0bc520ad8
gracekarinn <gracekarina12@gmail.com>
2025-02-10 22:23:01 +0700

    feat: enhance navbar with logo and updated buttons

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,20 +1,51 @@
-import React from 'react'
+"use client";
+
 import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
-    return (
-      <nav className="w-full border-b border-[#2B2B2B] bg-white flex justify-between items-center px-[42px] py-[21px]">
+  const router = useRouter();
+
+  return (
+    <nav className="w-full border-b border-neutral-800 bg-white">
+      <div className="flex justify-between items-center px-[42px]  py-[21px]">
         <div className="flex items-center gap-8">
-          <span className="text-xl text-black font-semibold">Logo</span>
-          <div className="flex gap-6">
-            <Link href="/" className="text-base text-[#2B2B2B]">Home</Link>
-            <Link href="/verify" className="text-base text-[#2B2B2B]">Verify</Link>
+          <Link href="/" className="flex items-center">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={25}
+              height={32}
+              className="cursor-pointer"
+            />
+          </Link>
+          <div className="flex ml-6 gap-10">
+            <Link href="/" className="text-base text-neutral-800 hover:text-teal-500 transition-colors">
+              Home
+            </Link>
+            <Link href="/verify" className="text-base text-neutral-800 hover:text-teal-500 transition-colors">
+              Verify
+            </Link>
           </div>
         </div>
         <div className="flex items-center gap-4">
-          <button className="px-4 py-2 text-[#2B2B2B] border border-[#21C0B3] rounded">Log in</button>
-          <button className="px-4 py-2 bg-teal-500 text-black rounded">Register</button>
+          <Button
+            variant="outline"
+            className="border-teal-500 text-[#1B9E93]"
+            onClick={() => router.push('/login')}
+          >
+            Log in
+          </Button>
+          <Button
+            className="bg-teal-500 font-bold text-white hover:bg-teal-600"
+            onClick={() => router.push('/register')}
+          >
+            Get Started
+          </Button>
         </div>
-      </nav>
-    );
-  };
+      </div>
+    </nav>
+  );
+};
