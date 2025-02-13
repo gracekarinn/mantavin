3f55618b0cc81c4f903c71a11305a9b75f0d1d73
gracekarinn <gracekarina12@gmail.com>
2025-02-13 23:42:26 +0700

    feat: update import statement

--- frontend/src/modules/DashboardModule/index.tsx
+++ frontend/src/modules/DashboardModule/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { OverviewPageSection } from './sections/OverviewPageSection';
-import EmployeesPageSection from './sections/EmployeesPageSection';
+import { EmployeesPageSection } from './sections/EmployeesPageSection';
 import { ProofOfWorkSection } from './sections/ProofOfWorkSection';
 
 export const OverviewPageModule = () => {
