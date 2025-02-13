f1a3aabef19efbeb37e3e49087b860bd95f2861f
gracekarinn <gracekarina12@gmail.com>
2025-02-13 20:31:20 +0700

    refactor: update navbar import path

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/Navbar/Navbar";
+import { Navbar } from "@/components/Navbar/Navbar";
 import { Toaster } from "../components/ui/sonner";
 import { Providers } from "@/providers/provider";
 
