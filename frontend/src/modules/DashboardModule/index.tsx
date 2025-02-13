a6136b2c71a1cd436a76cf339295af25f4feaff4
gracekarinn <gracekarina12@gmail.com>
2025-02-13 23:23:46 +0700

    feat: add module

--- frontend/src/modules/DashboardModule/index.tsx
+++ frontend/src/modules/DashboardModule/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { OverviewPageSection } from './sections/OverviewPageSection';
+import EmployeesPageSection from './sections/EmployeesPageSection';
+import { ProofOfWorkSection } from './sections/ProofOfWorkSection';
 
 export const OverviewPageModule = () => {
     return <OverviewPageSection />;
+}
+
+export const EmployeesPageModule = () => {
+    return <EmployeesPageSection />;
+}
+
+export const ProofOfWorkPageModule = () => {
+    return <ProofOfWorkSection />;
 }
