fe23c34e3866b8e1581949528d770d36ad9d35b5
gracekarinn <gracekarina12@gmail.com>
2025-02-13 20:31:46 +0700

    feat: add constants for proof of work requests and yearly/quarterly data

---
+++ frontend/src/modules/DashboardModule/constant.ts
@@ -0,0 +1,39 @@
+export const proofOfWorkRequests = [
+    {
+        employeeName: "Suryadi Hasan",
+        reason: "Government administration",
+    },
+    {
+        employeeName: "Tatang Suryadi",
+        reason: "Government administration",
+    },
+    {
+        employeeName: "Titi Kurmasari",
+        reason: "Job application",
+    },
+    {
+        employeeName: "Popo",
+        reason: "Scholarship application",
+    },
+];
+
+export const yearlyData = [
+    { name: "Jan", value: 65 },
+    { name: "Feb", value: 72 },
+    { name: "Mar", value: 68 },
+    { name: "Apr", value: 80 },
+    { name: "May", value: 85 },
+    { name: "Jun", value: 78 },
+    { name: "Jul", value: 90 },
+    { name: "Aug", value: 88 },
+    { name: "Sep", value: 85 },
+    { name: "Oct", value: 92 },
+    { name: "Nov", value: 88 },
+    { name: "Dec", value: 95 },
+];
+
+export const quarterlyData = [
+    { name: "Month 1", value: 75 },
+    { name: "Month 2", value: 82 },
+    { name: "Month 3", value: 88 },
+];
