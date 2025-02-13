d857f19de7f771240fcb2ef0ad09746a14b2bead
gracekarinn <gracekarina12@gmail.com>
2025-02-13 20:31:33 +0700

    feat: add component with sidebar and main content area

---
+++ frontend/src/components/layout/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Navbar from "@/components/Navbar/Navbar";
+import Sidebar from "@/components/Sidebar/Sidebar";
+
+export const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen bg-[#F0FFFA]">
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
